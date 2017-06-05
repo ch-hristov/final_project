1914bdfe0a37effd8a48ebb49fc06a9db8c94c31
= <hristohristov54@gmail.com>
2017-06-05 15:59:14 +0300

    Correct coloring of vertices

--- final_uni_project/Vertex.cs
+++ final_uni_project/Vertex.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows.Media.Media3D;
 
@@ -23,14 +24,21 @@ namespace final_uni_project
             set
             {
                 _edges = value;
-                IsReceiver = Edges.All(x => !double.IsNegativeInfinity(x.Weight));
+
             }
         }
 
         public Vertex(int id)
         {
             ID = id;
-            this.Edges = new List<Edge>();
+            var collection = new ObservableCollection<Edge>();
+
+            collection.CollectionChanged += (a, b) =>
+            {
+                IsReceiver = Edges.All(x => x.Target.ID == x.Source.ID || !double.IsNegativeInfinity(x.Weight));
+            };
+
+            this.Edges = collection;
         }
     }
 }
